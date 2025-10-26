682711ab42dcee055d857694c7a526b62c4ffc4f
JuanCode <jucacar28@hotmail.com>
2025-10-26 13:41:00 -0500

    feat: make logo much larger
    - Mobile: w-32 h-32 (128px)
    - Desktop: w-48 h-48 (192px)
    - Significantly more prominent logo size

--- impact-compendium-app/Frontend/src/pages/Login.tsx
+++ impact-compendium-app/Frontend/src/pages/Login.tsx
@@ -45,7 +45,7 @@ export const Login: React.FC = () => {
           <img 
             src="/logo.svg" 
             alt="Impact Compendium Logo" 
-            className="w-24 h-24 lg:w-32 lg:h-32"
+            className="w-32 h-32 lg:w-48 lg:h-48"
           />
         </div>
 
